829cb9fe47acfe1802e37229e931186466cde11a
rodriguezroberto <rdrgzmndz.sm0101@gmail.com>
2022-02-26 20:24:59 -0600

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
 import quotes from './quotes.json';
-import { useState } from 'react';
 import QuoteBox from './components/QuoteBox';
 
 function App() {
